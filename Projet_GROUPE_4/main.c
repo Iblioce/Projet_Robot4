6a00207120edeea7276217f3f760201121537402
thomas <thomas.renault@groupe-esigelec.org>
2026-09-22 15:17:44 +0200

    fix : Avancement en ligne droite

--- Projet_GROUPE_4/main.c
+++ Projet_GROUPE_4/main.c
@@ -1,95 +1,63 @@
 #include <msp430.h>
- 
- 
-// void config_timer1(void)
-// {
-//   TA1CTL = 0| (TASSEL_2 | ID_1);// source SMCLK, prédiv
-//   TA1CCR0 = 6555;
-//   TA1CCR1= 4951; // voir texte
-//   // TA1CCR2 = 4000;
-//   TA1CTL |= TAIE;// autorisation interruption TAIE
-//   TA1CTL |= MC_1;// comptage en mode up    
-//   TA1CCTL2 |= OUTMOD_7;// activation du signal      
-// }
- 
 
+// unsigned int memo_capt=0;
+// unsigned int capt;
+// unsigned int diff_capt;
+int compteur_0 = 0;
+int compteur_1 = 0;
 
  
- 
-// void main(void)
-// {
-//   WDTCTL = WDTPW | WDTHOLD; // desactivation Watch Dog Timer
+ #pragma vector = PORT2_VECTOR
+__interrupt void ma_interrupt_opto_0(void)
+{
 
-// // Button
+  if((P2IFG & BIT0)==BIT0){
+    compteur_0 = compteur_0 + 1;
+    P2IFG &= ~(BIT0); 
 
+    if(compteur_0==192){
+      arret();
+    }
+  }
 
-
-//  P1SEL &= ~BIT3;  
-//  P1SEL2 &= ~BIT3; 
- 
-//   P2SEL &= ~( BIT0 | BIT1 | BIT2 | BIT3 | BIT4| BIT5 );  // Select I/O function for red LED
-//   P2SEL2 &= ~( BIT0 | BIT1 | BIT3 | BIT2 | BIT4 | BIT5 );
-//   P2SEL |= (BIT2 | BIT4);
- 
-//   P2DIR |= (BIT1 | BIT2 | BIT4 | BIT5);
-//   P2DIR &= ~(BIT2 | BIT4);
- 
-  
-//   P2OUT |= (BIT5 | BIT1);
- 
-//   BCSCTL1 = CALBC1_1MHZ;          // frequence d’horloge 1MHz
-//   DCOCTL = CALDCO_1MHZ;           //    "
-  
-//   config_timer1();
-
- 
- 
-//   P2IE |= (BIT0|BIT3);     // initialisation de l'interruption
-//   P2IES |= (BIT0|BIT3);    // interruption sur front descendant
-//   P2IFG &= ~(BIT0|BIT3); // RAZ flag d’interruption
-//   // __enable_interrupt();
-
-//    P2OUT |= (BIT2 | BIT4 | BIT1 | BIT5);
-//   while (1); // boucle infinie
-// }
-
-
-
-// // #pragma vector=PORT2_VECTOR
-// // __interrupt void TIMER0_ISR(void)
-// // {  
-// //   // 
-// //   //  P2IFG &= ~(BIT0|BIT3);
-
-// //  if(P2IFG & BIT0){
-// //     P2IFG &= ~BIT0;
-// //  }
-// //  if(P2IFG & BIT3){
-// //   P2IFG &= ~BIT3;
-// //  }
-// //   }
-
-void config_signal(){
-
-  BCSCTL1 = CALBC1_1MHZ;          // frequence d’horloge 1MHz
-  DCOCTL = CALDCO_1MHZ;
-  TA1CTL = 0| (TASSEL_2 | ID_1);// source SMCLK, prédiv
-  TA1CTL |= TAIE; // autorisation interruption TAIE
-  TA1CTL |= MC_1; // comptage en mode up  
-
-  //roue1
-  TA1CCTL2 |= OUTMOD_7;// activation du signal   1ere roue 
-  TA1CCR0 = 2000;
-  //TA1CCR1 = 2000;
-  TA1CCR2 = 1500;
-
-  //roue2
-  TA1CCR1 = 2000;
-  TA1CCTL1 |= OUTMOD_7;// activation du signal  2e roue
+  if((P2IFG & BIT3)==BIT3) {
+    compteur_1 = compteur_1 + 1;
+    P2IFG &= ~(BIT3);
+  } 
 
 }
+void config_signal(){
+
+  TA1CTL = 0| (TASSEL_2 | ID_3); // source SMCLK, prédiv
+  //TA1CTL |= TAIE; // autorisation interruption TAIE
+  TA1CTL |= MC_1; // comptage en mode up  
+
+  //roue1 - gauche
+  TA1CCTL2 |= OUTMOD_7; // activation du signal   1ere roue 
+  TA1CCTL1 |= OUTMOD_7; // activation du signal  2e roue 
+  TA1CCR0 = 2000;
+  TA1CCR2 = 1000;
+
+  //roue2 - droite
+  TA1CCR1 = TA1CCR2 * (0.964);
+
+}
+
+void arret(){
+  TA1CCR2 = 0;
+  TA1CCR1 = 0;
+}
+
+/*void ajuster_vitesse(){
+  TA1CCR2 = 1000;
+  TA1CCR2 = TA1CCR2*(compteur_0/compteur_1);
+}*/
+
 int main(void){
-  WDTCTL = WDTPW +WDTHOLD;
+  WDTCTL = WDTPW +WDTHOLD; // desactivation Watch Dog Timer
+  
+  BCSCTL1 = CALBC1_1MHZ;          // frequence d’horloge 1MHz
+  DCOCTL = CALDCO_1MHZ;
   //config
   /* P2.2 et P2.4 en mode timer
      P2.0, P2.1, P2.3, P2.5  en mode I/O
@@ -102,23 +70,16 @@ int main(void){
   P2DIR &= ~(BIT0|BIT3); // entrees à 0
   P2DIR |= (BIT1|BIT2|BIT4|BIT5); // sorties à 1 
 
+  // Gestion des Interruptions
+  P2IE |= (BIT0|BIT3); 
+  P2IES |=(BIT0|BIT3);
+
   P2OUT &= ~(BIT5);
   P2OUT |= BIT1;
 
+  P2IFG &= ~(BIT0|BIT3);
+
   config_signal();
-
- /* BCSCTL1 = CALBC1_1MHZ;          // frequence d’horloge 1MHz
-  DCOCTL = CALDCO_1MHZ;           //    "
-
-  TA1CTL = 0| (TASSEL_2 | ID_1);// source SMCLK, prédiv
-  TA1CCR0 = 2000;
-  //TA1CCR1 = 2000;
-  TA1CCR2 = 1500;
-  TA1CTL |= TAIE; // autorisation interruption TAIE
-  TA1CTL |= MC_1; // comptage en mode up  
-  TA1CCTL2 |= OUTMOD_7;// activation du signal   1ere roue 
-  TA1CCTL1 |= OUTMOD_7;// activation du signal  2e roue
-  */
+  __enable_interrupt();
+  while(1);
 }
-  
-  
