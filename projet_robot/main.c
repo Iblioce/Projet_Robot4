b5ee79e8ad7280138bdbddf7a3404f05cfb25e2a
Thomas <114859927+Iblioce@users.noreply.github.com>
2026-09-22 14:01:39 +0200

    Update timer and interrupt configuration in main.c
    Refactor timer configuration and interrupt handling for better clarity and functionality.

--- projet_robot/main.c
+++ projet_robot/main.c
@@ -1,52 +1,71 @@
 #include <msp430.h>
 
-void config_timer1(void)
+// unsigned int memo_capt=0;
+// unsigned int capt;
+// unsigned int diff_capt;
+int compteur_0 = 0;
+int compteur_1 = 0;
+
+ 
+ #pragma vector = PORT2_VECTOR
+__interrupt void ma_interrupt_opto_0(void)
 {
-   TA1CTL = 0| (TASSEL_2 | ID_1);// source SMCLK, prédiv
-  TA1CCR0 = 6555;
-  TA1CCR1= 4951; // voir texte
-  TA1CCTR2 = 4000;
-  TA1CTL |= TAIE;// autorisation interruption TAIE
-  TA1CTL |= MC_1;// comptage en mode up     
-  TA1CCTL1 |= OUTMOD_7;// activation du signal      
+
+  if((P2IFG & BIT0)){
+    compteur_0 = compteur_0 + 1;
+    P2IFG &= ~(BIT0);
+  }
+
+  if((P2IFG & BIT3) ) {
+    compteur_1 = compteur_1 + 1;
+    P2IFG &= ~(BIT3);
+  }
+
+}
+void config_signal(){
+
+  BCSCTL1 = CALBC1_1MHZ;          // frequence d’horloge 1MHz
+  DCOCTL = CALDCO_1MHZ;
+  TA1CTL = 0| (TASSEL_2 | ID_1); // source SMCLK, prédiv
+  TA1CTL |= TAIE; // autorisation interruption TAIE
+  TA1CTL |= MC_1; // comptage en mode up  
+
+  //roue1 - gauche
+  TA1CCTL2 |= OUTMOD_7; // activation du signal   1ere roue
+  TA1CCTL1 |= OUTMOD_7; // activation du signal  2e roue
+  TA1CCR0 = 2000;
+  TA1CCR2 = 1419;
+
+  //roue2 - droite
+  TA1CCR1 =1400;
+
+
 }
 
-// #pragma vector=TIMER0_A1_VECTOR
-// __interrupt void TIMER0_ISR(void)
-// {
-//     if ((TA0CTL & TAIFG) == TAIFG)
-//     {
-//         P1OUT &= ~(BIT2);
-//         TA0CTL &= MC_0; //arret du comptage
-//         TA0CTL &= ~(TAIFG);
-//     }
-// }
-
-
-void main(void)
-{
-  WDTCTL = WDTPW | WDTHOLD; // desactivation Watch Dog Timer
-
-  P2SEL &= ~( BIT1 | BIT2 | BIT4| BIT5 );  // Select I/O function for red LED
-  P2SEL2 &= ~( BIT1 | BIT3 | BIT2 | BIT4 | BIT5 );
-  P2SEL |= (BIT2 | BIT4);
-  
-  P2DIR |= (BIT1 | BIT2 | BIT4 | BIT5); //
-  P2DIR &= ~(BIT2 | BIT4);
-
-  P2OUT &= ~(BIT2 | BIT4);
-  P2OUT |= (BIT5 | BIT1); 
-
-  /*BCSCTL1 = CALBC1_1MHZ;          // frequence d’horloge 1MHz
-  DCOCTL = CALDCO_1MHZ;           //    "
+int main(void){
+  WDTCTL = WDTPW +WDTHOLD; // desactivation Watch Dog Timer
+  //config
+  /* P2.2 et P2.4 en mode timer
+     P2.0, P2.1, P2.3, P2.5  en mode I/O
   */
-  config_timer1();
-  
-   
+  P2SEL &= (BIT0|BIT1|BIT3|BIT5); // mode I/O à 0
+  P2SEL |= (BIT2|BIT4); // mode timer à 1
+  P2SEL2 &= ~(BIT0|BIT1|BIT2|BIT3|BIT4|BIT5); // timer et I/O à 0
 
-  P2IE |= (BIT3|BIT0);     // initialisation de l'interruption
-  P2IES |= (BIT3|BIT0);    // interruption sur front descendant
-  P2IFG &= ~(BIT0|BIT3); // RAZ flag d’interruption
-  __enable_interrupt();
-  while (1);// boucle infinie
+  //Directions : 0 entree et 1 sortie
+  P2DIR &= ~(BIT0|BIT3); // entrees à 0
+  P2DIR |= (BIT1|BIT2|BIT4|BIT5); // sorties à 1
+
+  // Gestion des Interruptions
+  P2IE |= (BIT0|BIT3);
+  P2IES |=(BIT0|BIT3);
+
+  P2OUT &= ~(BIT5);
+  P2OUT |= BIT1;
+
+  P2IFG &= ~(BIT0|BIT3);
+
+  config_signal();
+  // __enable_interrupt();
+  while(1);
 }
