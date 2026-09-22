4cd75a3a13e32ddacc9030cbec17950a47b89bb8
thomas <thomas.renault@groupe-esigelec.org>
2026-09-22 17:26:26 +0200

    gros commit

--- Projet_GROUPE_4/main.c
+++ Projet_GROUPE_4/main.c
@@ -5,8 +5,36 @@
 // unsigned int diff_capt;
 int compteur_0 = 0;
 int compteur_1 = 0;
-
+int compteur_angle = 0;
+//typedef enum {DROITE, GAUCHE, face} sens ;
  
+ void arreter(){
+  TA1CCR2 = 0;
+  TA1CCR1 = 0;
+}
+
+void avancer(){
+  P2OUT &= ~(BIT1);
+  P2OUT |= BIT5;
+}
+
+void reculer(){
+  P2OUT &= ~(BIT5);
+  P2OUT |= BIT1;
+}
+
+void tourner_droite(){
+  P2OUT &= ~(BIT1);
+  P2OUT &= ~BIT5;
+  compteur_angle = 0;
+  //TA1CCR2 = 0;
+}
+
+void tourner_gauche(){
+  P2OUT |= BIT1;
+  P2OUT |= BIT5;
+  compteur_angle = 0;
+}
  #pragma vector = PORT2_VECTOR
 __interrupt void ma_interrupt_opto_0(void)
 {
@@ -14,17 +42,33 @@ __interrupt void ma_interrupt_opto_0(void)
   if((P2IFG & BIT0)==BIT0){
     compteur_0 = compteur_0 + 1;
     P2IFG &= ~(BIT0); 
-
-    if(compteur_0==192){
-      arret();
+    if(compteur_0==96){
+      reculer();
+    }
+    compteur_angle++;
+    //ajustement vitesse
+    if(compteur_0==20){
+      ajuster_vitesse();
     }
   }
 
   if((P2IFG & BIT3)==BIT3) {
     compteur_1 = compteur_1 + 1;
+    compteur_angle++;
     P2IFG &= ~(BIT3);
   } 
+  
+    if((compteur_angle>=14)){
+      avancer();
+    }
+}
 
+void init_vitesse(){
+  //roue1 - droite
+  TA1CCR2 = 1000;
+
+  //roue2 - gauche
+  TA1CCR1 = TA1CCR2 * (0.964);
 }
 void config_signal(){
 
@@ -32,26 +76,24 @@ void config_signal(){
   //TA1CTL |= TAIE; // autorisation interruption TAIE
   TA1CTL |= MC_1; // comptage en mode up  
 
-  //roue1 - gauche
+
   TA1CCTL2 |= OUTMOD_7; // activation du signal   1ere roue 
   TA1CCTL1 |= OUTMOD_7; // activation du signal  2e roue 
   TA1CCR0 = 2000;
-  TA1CCR2 = 1000;
-
-  //roue2 - droite
-  TA1CCR1 = TA1CCR2 * (0.964);
-
 }
-
-void arret(){
-  TA1CCR2 = 0;
-  TA1CCR1 = 0;
-}
-
-/*void ajuster_vitesse(){
+void ajuster_vitesse(){
+int min = 0;
+int max = 0;
+  if(compteur_0 > compteur_1){
+    max = compteur_0;
+    min = compteur_1;
+  }else{
+    max = compteur_1;
+    min = compteur_0;
+  }
   TA1CCR2 = 1000;
-  TA1CCR2 = TA1CCR2*(compteur_0/compteur_1);
-}*/
+  TA1CCR2 = TA1CCR2*(max/min);
+}
 
 int main(void){
   WDTCTL = WDTPW +WDTHOLD; // desactivation Watch Dog Timer
@@ -74,12 +116,11 @@ int main(void){
   P2IE |= (BIT0|BIT3); 
   P2IES |=(BIT0|BIT3);
 
-  P2OUT &= ~(BIT5);
-  P2OUT |= BIT1;
-
+  //avancer();
   P2IFG &= ~(BIT0|BIT3);
-
   config_signal();
+  tourner_droite();
+  init_vitesse();
   __enable_interrupt();
   while(1);
 }
