ed9c52b91fda43cdf2ae4a658c83fd8c48777712
Thomas <114859927+Iblioce@users.noreply.github.com>
2026-09-24 12:33:01 +0200

    feat : homologation - attention problème sur l'interuption depuis que nous avons changé de robot

--- Projet_GROUPE_4/main.c
+++ Projet_GROUPE_4/main.c
@@ -5,7 +5,7 @@ int compteur_0 = 0;
 int compteur_1 = 0;
 int compteur_angle = 0;
 
-typedef enum { AVANCER, TOURNER, NB_ETATS} ETAT;
+typedef enum { AVANCER, ARRET, NB_ETATS} ETAT;
 typedef enum { CAPT_ON, CAPT_OFF, NB_EVENTS} EVENT;
 
 typedef void (*Action)(void);
@@ -31,6 +31,15 @@ int get_capt(){
  void arreter(){
   TA1CCR2 = 0;
   TA1CCR1 = 0;
+}
+
+void init_vitesse(){
+  //roue2 - gauche
+  TA1CCR1 = 1000 ;
+  //roue1 - droite
+  TA1CCR2 = TA1CCR1 * 0.971;
+
+  
 }
 
 void avancer(){
@@ -75,12 +84,12 @@ typedef struct {
 
 Transition tab_transition [NB_ETATS][NB_EVENTS]= {
   [AVANCER] = {
-    [CAPT_OFF] = {AVANCER, avancer},
-    [CAPT_ON] = {TOURNER, tourner_gauche}
+    [CAPT_OFF] = {AVANCER, init_vitesse},
+    [CAPT_ON] = {ARRET, arreter}
   },
-  [TOURNER] = {
-    [CAPT_OFF] = {AVANCER, avancer},
-    [CAPT_ON] = {TOURNER, tourner_gauche}
+  [ARRET] = {
+    [CAPT_OFF] = {AVANCER, init_vitesse},
+    [CAPT_ON] = {ARRET, arreter}
   }
 };
 
@@ -91,6 +100,7 @@ __interrupt void ma_interrupt_opto_0(void)
   if((P2IFG & BIT0)){
     compteur_0 = compteur_0 + 1;
     P2IFG &= ~(BIT0);
+    
   }
 
   if((P2IFG & BIT3) ) {
@@ -106,25 +116,13 @@ void config_signal(){
   TA1CTL |= TAIE; // autorisation interruption TAIE
   TA1CTL |= MC_1; // comptage en mode up  
 
-  //roue1 - gauche
   TA1CCTL2 |= OUTMOD_7; // activation du signal   1ere roue
   TA1CCTL1 |= OUTMOD_7; // activation du signal  2e roue
   TA1CCR0 = 2000;
-  TA1CCR2 = 1419;
-
-  //roue2 - droite
-  TA1CCR1 =1400;
-
-
+  init_vitesse();
 }
 
-void init_vitesse(){
-  //roue1 - droite
-  TA1CCR2 = 1000;
 
-  //roue2 - gauche
-  TA1CCR1 = TA1CCR2 * (0.964);
-}
 
 void ajuster_vitesse(){
   if(compteur_0 > compteur_1){
@@ -155,19 +153,23 @@ int main(void){
   P2SEL |= (BIT2|BIT4); // mode timer à 1
   P2SEL2 &= ~(BIT0|BIT1|BIT2|BIT3|BIT4|BIT5); // timer et I/O à 0
 
+  P1SEL &= ~(BIT0|BIT6);
+  P1SEL2 &= ~(BIT0|BIT6);
+
   //Directions : 0 entree et 1 sortie
   P2DIR &= ~(BIT0|BIT3); // entrees à 0
   P2DIR |= (BIT1|BIT2|BIT4|BIT5); // sorties à 1
+  P1DIR |= (BIT0|BIT6);
+
+  P1OUT &= ~ (BIT0|BIT6);
 
   // Gestion des Interruptions
   P2IE |= (BIT0|BIT3);
   P2IES |=(BIT0|BIT3);
 
-  P2OUT &= ~(BIT5);
-  P2OUT |= BIT1;
-
   P2IFG &= ~(BIT0|BIT3);
 
+  avancer(); 
   config_signal();
 
   // Table de transition
@@ -175,12 +177,18 @@ int main(void){
   EVENT event;
   Transition trs;
   Aff_Init();
+  
   //__enable_interrupt();
   while(1){
-    event = get_event();
-    trs = tab_transition[etat][event];
-    trs.action();
-    etat=trs.etat_suivant;
+    if(compteur_0 >= 101){
+      arreter();
+    }else {
+      event = get_event();
+      trs = tab_transition[etat][event];
+      trs.action();
+      etat=trs.etat_suivant;
+    }
+    
     //ajuster_vitesse();
   }
 
